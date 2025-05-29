cec6dbb0f355162a9e2393900ec74f52d46b6f62
Bhargav <pvb.8394@gmail.com>
2025-05-29 11:02:58 -0500

    Initial Commit

---
+++ src/main/java/com/billspltr/BillSpltrApplication.java
@@ -0,0 +1,13 @@
+package com.billspltr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSpltrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillSpltrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/billspltr/BillSpltrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billspltr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSpltrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
